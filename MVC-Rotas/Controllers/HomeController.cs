f1ed9ee7f60dec5486a63079cca9d5fd3d6d4eb1
maikiely.gomes <maikiely.gomes@sefaz.es.gov.br>
2019-11-29 17:19:05 -0300

    Trabalhando com rotas no MVC

---
+++ MVC-Rotas/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using MVC_Rotas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Rotas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEnumerable<Noticia>todasAsNoticias;
+
+        public HomeController()
+        {
+            todasAsNoticias = new Noticia().TodasAsNoticias().OrderByDescending(x => x.data);
+        }
+        public ActionResult Index()
+        {
+            var ultimasNoticias = todasAsNoticias.Take(3);
+            var todasCategorias = todasAsNoticias.Select(x => x.categoria).Distinct().ToList();
+
+            ViewBag.Categoria = todasCategorias;
+            return View(ultimasNoticias);
+        }
+        
+        public ActionResult TodasAsNoticias()
+        {
+            return View(todasAsNoticias);
+        }
+
+        public ActionResult MostraNoticia(int noticiaID, string categoria, string titulo)
+        {
+            return View(todasAsNoticias.FirstOrDefault(x => x.noticiaID == noticiaID));
+        }
+
+        public ActionResult MostraCategoria(string categoria)
+        {
+            var categoriaEspecifica = todasAsNoticias.Where(x => x.categoria.ToLower() == categoria.ToLower()).ToList();
+            ViewBag.categoria = categoria;
+            return View(categoriaEspecifica);
+        }
+    }
+}
---
+++ MVC-Rotas/Models/Noticia.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Rotas.Models
+{
+    public class Noticia
+    {
+        public int noticiaID { get; set; }
+        public string titulo { get; set; }
+        public string categoria { get; set; }
+        public string conteudo { get; set; }
+        public DateTime data { get; set; }
+
+        public IEnumerable<Noticia> TodasAsNoticias()
+        {
+            var retorno = new Collection<Noticia>
+                {
+                    new Noticia
+                        {
+                            noticiaID = 1,
+                            categoria = "Esportes",
+                            titulo = "Felipe Massa ganha F1",
+                            conteudo = "Numa tarde de chuva Felipe Massa ganha F1 no Brasil...",
+                            data = new DateTime(2017,3,1)
+                        },
+                    new Noticia
+                        {
+                            noticiaID = 2,
+                            categoria = "Política",
+                            titulo = "Presidente assina convênios",
+                            conteudo = "Durante reunião o presidente Ismael Freitas assinou os convênios...",
+                            data = new DateTime(2017,3,1)
+                        },
+                    new Noticia
+                        {
+                            noticiaID = 3,
+                            categoria = "Política",
+                            titulo = "Vereador é eleito pela 4ª vez",
+                            conteudo = "Vereador Fabio Pratt é eleito pela quarta vez...",
+                            data = new DateTime(2017,3,1)
+                        },
+                    new Noticia
+                        {
+                            noticiaID = 4,
+                            categoria = "Esportes",
+                            titulo = "O tão sonhado titulo chegou",
+                            conteudo = "Em um jogo que levou os torcedores ao delirio...",
+                            data = new DateTime(2017,3,1)
+                        },
+                    new Noticia
+                        {
+                            noticiaID = 5,
+                            categoria = "Humor",
+                            titulo = "O Comediante Anderson Renato fará shou hoje",
+                            conteudo = "O comediante mais engraçados dos comentários do Youtube fará show...",
+                            data = new DateTime(2017,3,1)
+                        },
+                    new Noticia
+                        {
+                            noticiaID = 6,
+                            categoria = "Policial",
+                            titulo = "Tenente coronel Lucas Farias Santos assume o controle",
+                            conteudo = "Durante a retomada do morro o tenente coronel disse...",
+                            data = new DateTime(2017,3,1)
+                        },
+                    new Noticia
+                        {
+                            noticiaID = 7,
+                            categoria = "Esportes",
+                            titulo = "Atacante do Barcelona faz 4 gols",
+                            conteudo = "O atacante Lucas Farias Santos Messi, fez 4 gols e decidiu o titulo...",
+                            data = new DateTime(2017,3,1)
+                        }
+                };
+            return retorno;
+        }
+    }
+}
+
